10b9173005742ebb1612f7e2a72446910291a681
Igor Herson <igorhaf@gmail.com>
2025-01-05 20:32:29 +0000

    fix

---
+++ src/main/java/com/gestaopessoal/personal_manager/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.personalmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello, World! Bem-vindo ao Personal Manager!";
+    }
+}
