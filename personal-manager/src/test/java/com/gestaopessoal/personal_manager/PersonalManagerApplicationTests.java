3e0f9b1e23fd423d16f7d8ed04ff4a88188ff252
Igor Herson <igorhaf@gmail.com>
2025-01-05 11:28:47 -0300

    local changes

---
+++ personal-manager/src/test/java/com/gestaopessoal/personal_manager/PersonalManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaopessoal.personal_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
